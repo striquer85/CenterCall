ebb186918aff10dfb094f4110482b054ecb21300
Thomas <combarieu.t@gmail.com>
2024-09-27 14:52:50 +0200

    g

--- app/Views/Layout.php
+++ app/Views/Layout.php
@@ -8,6 +8,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="shortcut icon" type="image/png" href="/favicon.ico">
     <link rel="stylesheet" type="text/css" href="css/main.css">
+    
 
 </head>
 
