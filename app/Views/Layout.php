e054d6f8c15b0c9b3393ed7c2ba7794cad1214ae
Thomas <combarieu.t@gmail.com>
2024-09-27 14:39:32 +0200

    h

--- app/Views/Layout.php
+++ app/Views/Layout.php
@@ -1,4 +1,15 @@
 
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>CenterCall</title>
+    <meta name="description" content="The small framework with powerful features">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="shortcut icon" type="image/png" href="/favicon.ico">
 
+    <!-- STYLES -->
+
+  </head>
 
 <?= $this->renderSection('contenu')?>
