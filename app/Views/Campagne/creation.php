fbb6a1e6958808175211d5e6bf3d057ebc66a50f
Thomas <combarieu.t@gmail.com>
2024-10-04 14:16:35 +0200

    modif

--- app/Views/Campagne/creation.php
+++ app/Views/Campagne/creation.php
@@ -1,7 +1,24 @@
-<?= $this->extend('Layout')?>
-<?= $this->section('contenu')?>
+<?= $this->extend('Layout') ?>
+<?= $this->section('contenu') ?>
 
 
-<p>coucou</p>
 
-<?= $this->endSection()?>
+
+
+<form >
+<h2>Création de la campagne</h2>
+
+    <label for="name">Titre de la campagne :</label>
+
+    <input type="text" id="titre" name="titre" required
+        minlength="3" maxlength="25" size="18">
+
+        
+    <label for="uname">Texte descriptif :</label>
+    <input type="text" id="descriptif" name="descriptif" placeholder="Tapez votre descriptif : " 
+    minlength="3" maxlength="100" size="18">
+
+
+</form>
+
+<?= $this->endSection() ?>
