3b89363e53aa8e78612d4a4911889e805786e429
striquer85 <stephan.marcerou@gmail.com>
2024-11-29 14:23:45 +0100

    com du controleur du client

--- app/Controllers/Client.php
+++ app/Controllers/Client.php
@@ -12,6 +12,11 @@ class Client extends BaseController
     private $userModel;
     private $db;
 
+    /* ------------------------ creation du constructeur ------------------------ */
+    /*                       implementtation des model et db                      */
+    /* -------------------------------------------------------------------------- */
+
+
     public function __construct()
     {
 
@@ -21,9 +26,20 @@ class Client extends BaseController
         $this->userModel = model('UserModel');
         $this->db = db_connect();
     }
-
+    /* ------------------------ gestion des clients ------------------------ */
+    /* affichage des clients avec leur information et leur entreprise         */
+    /* -------------------------------------------------------------------------- */
     public function gestionclient()
     {
+
+        /* ------------------------ test de la methode admin ------------------------ */
+        /*test si utilisateur est admin                                               */
+        /*si il est admin il va aller vers le dashboard                               */
+        /*sinon il va aller vers la gestion des clients                               */
+        /*si il est connecte mais pas admin il va aller                               */
+        /*vers le dashboard des campagne associé au client                            */
+        /* ---------------------------------------------------------------------------*/
+
         $user = auth()->user();
 
         if (!$user->inGroup('admin')) {
@@ -36,6 +52,11 @@ class Client extends BaseController
         $client = $this->clientModel->findAll();
         return view('Client/gestion_admin', ['listeClients' => $client]);
     }
+    /* ---------------------------- ajout d'un client --------------------------- */
+    /* affichage de la page de creation d'un client                               */
+    /* affichage de la liste des utilisateurs                                     */
+    /* recuprerations des clients et des utilisateurs                             */
+    /* -------------------------------------------------------------------------- */
     public function ajout()
     {
         $user = auth()->user();
@@ -46,9 +67,16 @@ class Client extends BaseController
 
             return redirect()->to("gestion-campagnes/{$idClient['ID_CLIENT']}");
         }
+
         $listeUser = $this->userModel->findAll();
         return view('Client/creation', ['listeUser' => $listeUser]);
     }
+
+    /* -------------------------- creation d'un client -------------------------- */
+    /* recuprerations des clients et des utilisateurs                             */
+    /* creation d'un client dans la base de donnée                                */
+    /* redirection vers la gestion des clients                                    */
+    /* -------------------------------------------------------------------------- */
 
     public function create()
     {
@@ -64,7 +92,11 @@ class Client extends BaseController
         $this->clientModel->insert($data);
         return redirect()->to('gestion-clients');
     }
-
+    /* ------------------------- modification de client ------------------------- */
+    /* affichage de la page de modification d'un client                          */
+    /* affichage de la liste des utilisateurs                                     */
+    /* recuprerations des clients et des utilisateurs                             */
+    /* -------------------------------------------------------------------------- */
     public function modif($idClient)
     {
         $user = auth()->user();
@@ -82,7 +114,10 @@ class Client extends BaseController
             'listeUser' => $listeUser
         ]);
     }
-
+    /* --------------- aplication des modifications sur le client --------------- */
+    /* update des données du client dans la base de donnée                        */
+    /* redirection vers la gestion des clients                                    */
+    /* -------------------------------------------------------------------------- */
     public function update()
     {
         $user = auth()->user();
@@ -97,6 +132,18 @@ class Client extends BaseController
         $this->clientModel->save($dataClient);
         return redirect()->to('gestion-clients');
     }
+
+    /* ------------------------ suppression d'un client ------------------------ */
+    /* recuprerations des informations du client                                 */
+    /* recuprerations des utilisateurs                                           */
+    /* recuprerations des campagnes du client                                    */
+    /* recuprerations des questions de la campagne                               */
+    /* suppression des campagnes                                                 */
+    /* suppression des questions                                                 */
+    /* suppression des utilisateurs                                              */
+    /* suppression d'un client dans la base de donnée                            */
+    /* redirection vers la gestion des clients                                   */
+    /* --------------------------------------------------------------------------*/
 
     public function delete($idClient)
     {
