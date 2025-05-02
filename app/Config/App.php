3a68e466fa6256846c493933e18066d383a0a9c1
striquer85 <stephan.marcerou@gmail.com>
2025-05-02 14:47:41 +0200

    test

--- app/Config/App.php
+++ app/Config/App.php
@@ -16,7 +16,7 @@ class App extends BaseConfig
      *
      * E.g., http://example.com/
      */
-    public string $baseURL = 'http://centercall-stephan.com/';
+    public string $baseURL = 'http://centercall.local/';
     /**
      * Allowed Hostnames in the Site URL other than the hostname in the baseURL.
      * If you want to accept multiple Hostnames, set this.
